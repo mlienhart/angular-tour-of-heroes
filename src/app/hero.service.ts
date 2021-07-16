e240cda2197b448f3ac764f527caebaf006bc7b7
Martin Lienhart <martin.lienhart@outlook.com>
2021-07-16 21:11:24 +0200

    6

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
@@ -12,8 +13,14 @@ export class HeroService {
   constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    const heroes = of(HEROES);
+    const heroes = of(HEROES)
+      .pipe(map(data => data
+        .map(x => <Hero>
+          { id: x.id * 100, name: x.name.toLocaleUpperCase() }
+        )
+        .filter(x => x.id > 1300)));
     this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
+
 }
